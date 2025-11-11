028c7550a0d120165b307294966866c1cea858b7
Valentina Molina <valemolina226@gmail.com>
2025-11-11 19:42:17 -0300

    actualización

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-document.addEventListener('DOMContentLoaded', () => {
-  // Si querés forzar que ciertas páginas estén protegidas:
-  // Por ejemplo: si currentPath contiene '/pages/pedidos.html' => requireAuth()
-  // window.maluccaAuth && window.maluccaAuth.requireAuth();
-
-  // También podés hacer otras inicializaciones aquí
-});
